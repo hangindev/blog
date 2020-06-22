a2d4a4699fa1d248c7a53e91889c5e02e474aa54
hangindev <hangindev@gmail.com>
2020-06-22 20:20:56 +0800

    Add payment pointer.

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { getFavicon, imageBuilder } from '../lib/api';
+import { getDocuemntData, imageBuilder } from '../lib/api';
 
 const appleTouchIconWidths = [57, 76, 96, 120, 144, 195, 228];
 const faviconWidths = [192, 32, 96, 16];
@@ -7,41 +7,44 @@ const faviconWidths = [192, 32, 96, 16];
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    const iconUrl = await getFavicon();
-    return { ...initialProps, iconUrl };
+    const { icon, paymentPointer } = await getDocuemntData();
+    return { ...initialProps, icon, paymentPointer };
   }
 
   render() {
-    const { iconUrl } = this.props;
+    const { icon, paymentPointer } = this.props;
     return (
       <Html lang="en">
         <Head>
-          {iconUrl &&
+          {icon &&
             appleTouchIconWidths.map(w => (
               <link
                 key={w}
                 rel="apple-touch-icon"
                 sizes={`${w}x${w}`}
                 href={imageBuilder
-                  .image(iconUrl)
+                  .image(icon)
                   .width(w)
                   .height(w)
                   .url()}
               />
             ))}
-          {iconUrl &&
+          {icon &&
             faviconWidths.map(w => (
               <link
                 key={w}
                 rel="icon"
                 sizes={`${w}x${w}`}
                 href={imageBuilder
-                  .image(iconUrl)
+                  .image(icon)
                   .width(w)
                   .height(w)
                   .url()}
               />
             ))}
+          {paymentPointer && (
+            <meta name="monetization" content={paymentPointer} />
+          )}
         </Head>
         <body>
           <Main />
