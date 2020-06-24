e6b3cde34c5aa7d2f4f7db07081515900505181b
hangindev <hangindev@gmail.com>
2020-06-24 11:45:46 +0800

    Add sitemap.xml generation.

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -33,6 +33,7 @@ function buildSiteMap(pageFiles) {
     const $ = cheerio.load(htmlString);
     return $(`meta[property='og:url']`).attr('content');
   });
+  console.log(urls);
   const sitemap = `
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
   xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" 
@@ -40,15 +41,17 @@ function buildSiteMap(pageFiles) {
   xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" 
   xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" 
   xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-  ${urls.map(
-    url => `
+  ${urls
+    .map(
+      url => `
     <url>
-      <loc>${url}/loc>
+      <loc>${url}</loc>
       <changefreq>daily</changefreq>
       <priority>0.7</priority>
     </url>
     `
-  )}  
+    )
+    .join('')}  
 </urlset>;
 `;
   fs.writeFileSync(path.join('./.next/static', 'sitemap.xml'), sitemap);
